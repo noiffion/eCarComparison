ce817ec6a355d32c62b5cea995bf73824cc9f1db
David Kerekes <davkedav@gmail.com>
2020-07-23 21:45:56 +0200

    typescripted console colors

---
+++ server/misc/console.ts
@@ -0,0 +1,83 @@
+interface CnsFn {
+  (...args: string[]): void;
+}
+
+declare global {
+  interface Console {
+    oldInfo: CnsFn;
+    oldWarn: CnsFn;
+    oldError: CnsFn;
+  }
+}
+
+interface BaseLog {
+  icon: string;
+  whiteFg: string;
+  reset: string;
+  reverse: string;
+}
+
+interface Info extends BaseLog {
+  blueBg: string;
+  blueFg: string;
+}
+const info: Info = {
+ icon: '\u2139',
+ blueBg: '\x1b[44m',
+ blueFg: '\x1b[36m',
+ whiteFg: '\x1b[37m',
+ reset: '\x1b[0m',
+ reverse: '\x1b[7m'
+};
+
+console.oldInfo = console.info;
+console.info = function() {
+  const infoIcon = [info.blueBg + info.blueFg, info.icon, info.reset];
+  const msg = [info.blueFg, ...arguments, info.reset];
+  console.oldInfo(...infoIcon, ...msg);
+};
+
+
+interface Warn extends BaseLog {
+  yellowBg: string;
+  yellowFg: string;
+}
+const warn: Warn = {
+ icon: '\u26A0',
+ yellowBg: '\x1b[43m',
+ yellowFg: '\x1b[33m',
+ whiteFg: '\x1b[37m',
+ reset: '\x1b[0m',
+ reverse: '\x1b[7m'
+};
+
+console.oldWarn = console.warn;
+console.warn = function() {
+  const warnIcon: string[] = [warn.yellowBg + warn.whiteFg, warn.icon, warn.reset];
+  const msg = [warn.yellowFg, ...arguments, warn.reset];
+  console.oldWarn(...warnIcon, ...msg);
+};
+
+
+interface Error extends BaseLog {
+  redBg: string;
+  redFg: string;
+}
+const error: Error = {
+ icon: '✘',
+ redBg: '\x1b[41m',
+ redFg: '\x1b[31m',
+ whiteFg: '\x1b[37m',
+ reset: '\x1b[0m',
+ reverse: '\x1b[7m'
+};
+
+console.oldError = console.error;
+console.error = function() {
+  const errorIcon: string[] = [error.redBg + error.whiteFg, error.icon, error.reset];
+  const msg = [error.redFg, ...arguments, error.reset];
+  console.oldError(...errorIcon, ...msg);
+};
+
+
+export default console;
