ca43460589e2498da4defeb129d90ccf6e10f3da
David Kerekes <davkedav@gmail.com>
2020-07-24 08:50:09 +0200

    client/ structure setup

---
+++ client/src/Containers/CarBoard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CarBoard() {
+  return (
+    <main>
+      <h1>eCars</h1>
+    </main>
+  );
+}
+
+export default CarBoard;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import CarBoard from './Containers/CarBoard';
+import * as serviceWorker from './ServiceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={createBrowserHistory()}>
+      <CarBoard />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
