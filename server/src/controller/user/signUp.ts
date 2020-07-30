ccb5e1d01b3ce3bbcc0f87077134c93ba6fe80ae
David Kerekes <davkedav@gmail.com>
2020-07-30 08:23:02 +0200

    signUp() implemented

--- server/src/controller/user/signUp.ts
+++ server/src/controller/user/signUp.ts
@@ -1,11 +1,27 @@
 import { ControllerMethod } from '../interface';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import Users from '../../models/Users';
+const JWT_KEY = process.env.JWT_KEY || '';
 
 const signUp: ControllerMethod = async function (req, res) {
+  const { email, password } = req.body;
+  const user = await Users.findOne({ email: email });
+  if (user !== undefined) {
+    return res.status(409).send({ error: '409', message: 'User already exists' });
+  }
+  const pswdHash = bcrypt.hash(password, 10);
+  const newUser = new Users({
+    ...req.body,
+    password: pswdHash,
+  });
   try {
-    console.log('signUp');
+    const { _id } = await newUser.save();
+    const accessToken = jwt.sign({ _id }, JWT_KEY);
+    res.status(201).send({ accessToken });
   } catch (err) {
     console.error(err);
+    res.status(500).send({ error: err, message: 'Could not create user!' });
   }
 };
 
