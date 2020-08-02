e004fa505f53d6bbd1fd482043005d7931a6654f
David Kerekes <davkedav@gmail.com>
2020-08-02 10:46:37 +0200

    controller getOneCar() update

--- server/src/controller/car/getOneCar.ts
+++ server/src/controller/car/getOneCar.ts
@@ -1,10 +1,12 @@
 import { ControllerMethod } from '../interface';
 import Cars from '../../models/Cars';
+import { ICar } from '../../models/interfaces';
 
 const getOneCar: ControllerMethod = async function (req, res) {
   try {
+    const car: ICar = await Cars.findById(req.params.carId);
     res.status(200);
-    res.send('getOneCar');
+    res.send(car);
   } catch (err) {
     console.error(err);
     res.status(500);
