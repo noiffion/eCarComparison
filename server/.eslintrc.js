01a2f70a91b1bc5eb6d9675a9f2fb951ea12899e
David Kerekes <davkedav@gmail.com>
2020-09-17 12:33:34 +0200

    docs(.eslintrc.js): update tsconfigRootDir: __dirname

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  "root": true,
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
-  "plugins": ["@typescript-eslint"],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module",
-    "project": "./tsconfig.json",
-    "tsconfigRootDir": __dirname
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
   },
   rules: {
-    "linebreak-style": "off",
-    "no-console": [
-      "warn",
+    'linebreak-style': 'off',
+    'no-console': [
+      warn,
       {
-        "allow": ["oldInfo", "info", "oldWarn", "warn", "oldError", "error"]
+        allow: ['oldInfo', 'info', 'oldWarn', 'warn', 'oldError', 'error']
       }
     ]
   }
