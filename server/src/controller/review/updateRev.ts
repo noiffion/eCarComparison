a21c853eecda0bba38c0b2bfa03849bef48769fe
David Kerekes <davkedav@gmail.com>
2020-09-22 14:19:18 +0200

    feat(updateRev.ts): implement updateRev()

--- server/src/controller/review/updateRev.ts
+++ server/src/controller/review/updateRev.ts
@@ -3,8 +3,9 @@ import Reviews from '../../models/Reviews';
 
 const updateRev: ControllerMethod = async function (req, res) {
   try {
+    const updatedReview = await Reviews.findByIdAndUpdate(req.params.reviewId, req.body);
     res.status(200);
-    res.send('updateRev');
+    res.send(updatedReview);
   } catch (err) {
     console.error(err);
     res.status(500);
