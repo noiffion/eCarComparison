d104431c0aa83db60f2834be95b38844b94b66b9
David Kerekes <davkedav@gmail.com>
2020-09-23 01:27:33 +0200

    feat(createRev.ts): implement createRev()

--- server/src/controller/review/createRev.ts
+++ server/src/controller/review/createRev.ts
@@ -1,10 +1,15 @@
 import { ControllerMethod } from '../controller';
 import Reviews from '../../models/Reviews';
+import { IReview } from '../../models/models';
 
 const createRev: ControllerMethod = async function (req, res) {
   try {
+    await Reviews.create({ ...req.body });
+    const reviews: IReview[] = await Reviews.find({ carId: `${req.body.carId}` }, null, {
+      sort: { updatedAt: -1 },
+    });
     res.status(200);
-    res.send('createRev');
+    res.send(reviews);
   } catch (err) {
     console.error(err);
     res.status(500);
