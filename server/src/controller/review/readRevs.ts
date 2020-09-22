01f3a52099787af257d976a86b2e141986f05c0e
David Kerekes <davkedav@gmail.com>
2020-09-22 17:16:19 +0200

    feat(readRevs.ts): sort reviews from db

--- server/src/controller/review/readRevs.ts
+++ server/src/controller/review/readRevs.ts
@@ -4,7 +4,11 @@ import { IReview } from '../../models/models';
 
 const readRevs: ControllerMethod = async function (req, res) {
   try {
-    const reviews: IReview[] = await Reviews.find({ carId: `${req.params.carId}` });
+    const reviews: IReview[] = await Reviews.find(
+      { carId: `${req.params.carId}` },
+      null,
+      {sort: { updatedAt: -1 }},
+    );
     res.status(200);
     res.send(reviews);
   } catch (err) {
