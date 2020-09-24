150b81dbdd459432cb48d6b830f702eb53b34660
David Kerekes <davkedav@gmail.com>
2020-09-24 12:11:46 +0200

    feat(server): implement uploadProfilePic()

--- server/src/controller/user/uploadProfilePic.ts
+++ server/src/controller/user/uploadProfilePic.ts
@@ -1,12 +1,17 @@
 import { ControllerMethod } from '../controller';
-import * as AWS from '../../utils/aws';
 import Users from '../../models/Users';
 import { IUser } from '../../models/models';
 
 const uploadProfilePic: ControllerMethod = async function (req, res) {
   try {
+    const userId = req.body.userId;
+    delete req.body.userId;
+    const user: IUser = await Users.findByIdAndUpdate(userId, req.body, {
+      new: true,
+      useFindAndModify: false,
+    });
     res.status(200);
-    res.send();
+    res.send(user);
   } catch (err) {
     console.error(err);
     res.status(500);
