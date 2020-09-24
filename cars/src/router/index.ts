178607c1c6c43cac00e4cfff9438c774f9ec7fa7
David Kerekes <davkedav@gmail.com>
2020-09-24 21:49:48 +0200

    chore(cars/): set up Docker for cars/

--- cars/src/router/index.ts
+++ cars/src/router/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import controller from '../controller';
-import authCheck from '../utils/authCheck';
 const router = express.Router();
 
 router.get('/cars', controller.getECars);
